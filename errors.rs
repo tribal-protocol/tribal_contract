9a708514adf9d030250857383897c7fb3d4851bb
Chris <chris@purelogictek.com>
2022-08-21 13:31:24 -0400

    add unit tests around error messages

--- errors.rs
+++ errors.rs
@@ -41,3 +41,55 @@ impl fmt::Display for TribeError {
     }
 }
 
+#[cfg(test)]
+mod founder_tests {
+    use super::*;
+    use ink_lang as ink;
+
+    #[ink::test]
+    fn check_error_generates_correct_string()  {
+        //ASSIGN
+        let error = TribeError::ActiveTribeCannotAcceptAction;
+
+        //ACT
+        let description = ink_prelude::format!("{}", error);
+
+        //ASSERT
+        assert_eq!(description, "Active tribe cannot accept activity");
+    }
+
+    macro_rules! error_description_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[ink::test]
+            fn $name() {
+                //ASSIGN
+                let (error, expected) = $value;
+
+                //ACT
+                let description = ink_prelude::format!("{}", error);
+
+                //ASSERT
+                assert_eq!(description, expected);
+            }
+        )*
+        }
+    }
+    error_description_tests! {
+        test_amount_promised_is_zero: (TribeError::AmountPromisedIsZero, "Amount promised in pico must be greater than 0"),
+        test_can_not_invite_initial_founder: (TribeError::CanNotInviteInitialFounder, "The initial founder can not be invited to join their own tribe"),
+        test_founder_already_invited: (TribeError::FounderAlreadyInvited, "AccountId already exists as a Founder"),
+        test_founder_list_not_found: (TribeError::FounderListNotFound, "Tribe list of founders  not found"),
+        test_founder_rejected_invitation: (TribeError::FounderRejectedInvitation, "Founder already rejected invitation to tribe"),
+        test_founder_vote_action_pending: (TribeError::FounderVoteActionPending, "Founder has not taken an action on pending invitation"),
+        test_funding_already_completed: (TribeError::FundingAlreadyCompleted, "Founder has already completed funding"),
+        test_funding_amount_must_be_greater_than_zero: (TribeError::FundingAmountMustBeGreaterThanZero, "Funding amount must be greater than zero amount"),
+        test_not_a_founder: (TribeError::NotAFounder, "AccountId is not a Founder"),
+        test_not_initial_founder: (TribeError::NotInitialFounder, "AccountId is not the Initial Founder"),
+        test_tribe_is_defunct: (TribeError::TribeIsDefunct, "Tribe is defunct and cannot accept any more activity"),
+        test_tribe_is_locked: (TribeError::TribeIsLocked, "Tribe is locked due to founder activity"),
+    }
+
+}
+
+
